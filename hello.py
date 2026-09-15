28fef52794c1b725be503efcd94452879c5f38af
joshebel <joshebel@gmail.com>
2026-09-15 08:19:41 -0600

    Update hello.py with repo update note
    Added a note about repository update for scanning purposes.

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@
 
 CWE-79: user-controlled query parameter is written into HTML with no escaping.
 Do not use this pattern in real software.
+Repo update to trigger scanning
 """
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
