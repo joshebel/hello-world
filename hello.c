b5389f038a7339cc37fb11bfd84c634ecb1be668
joshebel <joshebel@gmail.com>
2026-09-18 15:06:35 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
 /* INTENTIONAL VULNERABILITY for code-scanner testing.
  * CWE-120 / CWE-121: classic stack buffer overflow via unbounded strcpy.
  * Do not use this pattern in real software.
- * Here is an update again
+ * Here is an update again again
  */
 void greet(char *name) {
     char buffer[16];
